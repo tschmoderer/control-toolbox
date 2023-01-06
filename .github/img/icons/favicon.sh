13a6d5548a60e7ab1cebc9a8749d6e2b254a1c68
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2023-01-06 19:55:22 +0100

    Create favicon.sh

---
+++ .github/img/icons/favicon.sh
@@ -0,0 +1,21 @@
+# Bash script to build the library icon in different sizes and color scheme
+#!/bin/bash
+
+name=rocket
+names=($name $name-white $name-bw)
+sizes=(16 32 64 128 256 512)
+
+for n in ${names[@]}; do
+    svg=$n.svg
+    res=
+    
+    for i in ${sizes[@]}; do
+        folder=$i"x"$i
+        mkdir -p $folder
+        res+=" $folder/$n.png"
+        inkscape $svg -o "$folder/$n.png" -w $i -h $i 
+    done
+
+    convert $res $n.ico
+    identify $n.ico
+done
