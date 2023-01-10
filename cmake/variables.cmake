b83d0f3f7715e7db40efb8cd26e15ecfecb6eb41
Schmoderer <timothee.schmoderer@netcourrier.com>
2023-01-10 16:35:02 +0100

    add cmake scripts

---
+++ cmake/variables.cmake
@@ -0,0 +1,7 @@
+# cmake script to define variables
+
+## Directories
+set(DOCS ${PROJECT_SOURCE_DIR}/docs)
+set(EXS ${PROJECT_SOURCE_DIR}/examples)
+set(SRC ${PROJECT_SOURCE_DIR}/src)
+set(TST ${PROJECT_SOURCE_DIR}/tests)
