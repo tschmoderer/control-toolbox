7eb9d146027380572bbb7bb78b9e0b4e9299dcfd
Schmoderer <timothee.schmoderer@netcourrier.com>
2023-01-11 21:05:43 +0100

    update include files

--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -16,5 +16,6 @@ set(CMAKE_BUILD_TYPE Debug)
 ## Directories
 set(DOCS ${PROJECT_SOURCE_DIR}/docs)
 set(EXS ${PROJECT_SOURCE_DIR}/examples)
+set(INC ${PROJECT_SOURCE_DIR}/includes)
 set(SRC ${PROJECT_SOURCE_DIR}/src)
 set(TST ${PROJECT_SOURCE_DIR}/tests)
